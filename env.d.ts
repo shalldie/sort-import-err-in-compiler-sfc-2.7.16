259d5556c43858a14551b472f57788b8bc010376
xieshuang <hi.xieshuang@gmail.com>
2024-03-19 19:55:54 +0800

    update

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="./types/afe-i18n" />
-/// <reference path="node_modules/@baidu/vue-i18n/env" />
-/// <reference path="node_modules/@baidu/vue-svg/env" />
